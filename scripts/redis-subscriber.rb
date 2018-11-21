74c4de4cb168d5a068bf6d65fccffb46cbfd3a37
Leonardo Crauss Daronco <leonardodaronco@gmail.com>
2018-11-21 09:35:42 -0200

    Filter only the events we need from redis

--- scripts/redis-subscriber.rb
+++ scripts/redis-subscriber.rb
@@ -7,6 +7,17 @@ require rails_environment_path
 
 Rails.logger.info "Starting"
 
+target_events = [
+  "archive_started",
+  "archive_ended",
+  "sanity_started",
+  "sanity_ended",
+  "process_started",
+  "process_ended",
+  "publish_started",
+  "publish_ended"
+]
+
 redis = Redis.new(host: ENV["BBB_REDIS_HOST"], port: ENV["BBB_REDIS_PORT"], db: ENV["BBB_REDIS_DB"])
 
 redis.subscribe("bigbluebutton:from-rap") do |on|
@@ -16,7 +27,9 @@ redis.subscribe("bigbluebutton:from-rap") do |on|
 
   on.message do |channel, message|
     Rails.logger.info "Received message from #{channel}: #{message}"
-    Recording.sync_from_redis(JSON.parse(message))
+    parsed_message = JSON.parse(message)
+    name = parsed_message["header"]["name"]
+    Recording.sync_from_redis(parsed_message) if target_events.include?(name)
   end
 
   on.unsubscribe do |channel, subscriptions|
