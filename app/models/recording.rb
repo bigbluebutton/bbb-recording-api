7fa0edf22ddbcc4bf590578f25f7e016f15b3e61
Calvin Walton <calvin.walton@kepstin.ca>
2018-11-22 06:45:44 -0500

    Ensure recording id prefix lookup always returns a relation

--- app/models/recording.rb
+++ app/models/recording.rb
@@ -3,7 +3,7 @@ class Recording < ApplicationRecord
   has_many :playback_formats, dependent: :destroy
 
   scope :with_recording_id_prefixes, lambda { |recording_ids|
-    return if recording_ids.empty?
+    return none if recording_ids.empty?
 
     rid_prefixes = recording_ids.map { |rid| sanitize_sql_like(rid, '|') + '%' }
     query_string = Array.new(recording_ids.length, "record_id LIKE ? ESCAPE '|'").join(' OR ')
