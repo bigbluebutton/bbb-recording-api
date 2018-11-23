b10be0e1a7572245a4d6fdd9d10155b351e103f3
Calvin Walton <calvin.walton@kepstin.ca>
2018-11-23 06:42:36 -0500

    Return playback formats in sorted order by format name
    This ensures that the order of formats displayed in integrations is
    consistent. The order itself is arbitrary.

--- app/models/playback_format.rb
+++ app/models/playback_format.rb
@@ -1,4 +1,5 @@
 class PlaybackFormat < ApplicationRecord
   belongs_to :recording
   has_many :thumbnails, dependent: :destroy
+  default_scope { order(format: :asc) }
 end
--- test/models/playback_format_test.rb
+++ test/models/playback_format_test.rb
@@ -1,7 +1,10 @@
 require 'test_helper'
 
 class PlaybackFormatTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'Playback formats are returned in sorted order' do
+    pfs = PlaybackFormat.where(recording: recordings(:out_of_order))
+
+    assert_equal pfs[0].format, 'a'
+    assert_equal pfs[1].format, 'b'
+  end
 end
