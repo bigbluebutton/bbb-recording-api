e40f213fdfe4683822e797aa8907d69f49d1e9d6
Leonardo Crauss Daronco <leonardodaronco@gmail.com>
2019-05-07 12:21:54 -0400

    Change the state of recordings after calls to publishRecordings

--- app/controllers/bigbluebutton_api_controller.rb
+++ app/controllers/bigbluebutton_api_controller.rb
@@ -38,10 +38,10 @@ class BigbluebuttonApiController < ApplicationController
 
     publish = params[:publish].casecmp('true').zero?
 
-    query = Recording.where(record_id: params[:recordID].split(','), state: 'published')
+    query = Recording.where(record_id: params[:recordID].split(','), state: %w[ published unpublished ])
     raise ApiError.new('notFound', 'We could not find recordings') if query.none?
 
-    query.where.not(published: publish).update_all(published: publish)
+    query.where.not(published: publish).update_all(published: publish, state: (publish ? 'published' : 'unpublished'))
 
     @published = publish
     render :publish_recordings
