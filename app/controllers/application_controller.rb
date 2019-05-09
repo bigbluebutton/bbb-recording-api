ee1b63f03c28a497293dbea17906d3ef650abbe6
Leonardo Crauss Daronco <leonardodaronco@gmail.com>
2019-05-09 10:53:34 -0400

    Move common controller methods to ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,31 @@
 class ApplicationController < ActionController::API
   include ActionView::Layouts
+
+  class ApiError < StandardError
+    attr_reader :key
+
+    def initialize(key, message)
+      super(message)
+      @key = key
+    end
+  end
+
+  before_action :checksum
+  rescue_from ApiError, with: :api_error
+
+  def api_error(exception)
+    @exception = exception
+    render :api_error
+  end
+
+  def checksum
+    raise ApiError.new('checksumError', 'You did not pass the checksum security check') if params[:checksum].blank?
+
+    query_string = request.query_string.gsub(/^checksum=#{params[:checksum]}&?/, '')
+    query_string = query_string.gsub(/&?checksum=#{params[:checksum]}/, '')
+    our_checksum = Digest::SHA1.hexdigest(action_name + query_string + ENV.fetch('BBB_SECRET'))
+    return if our_checksum == params[:checksum]
+
+    raise ApiError.new('checksumError', 'You did not pass the checksum security check')
+  end
 end
--- app/controllers/bigbluebutton_api_controller.rb
+++ app/controllers/bigbluebutton_api_controller.rb
@@ -1,16 +1,5 @@
 class BigbluebuttonApiController < ApplicationController
-  class ApiError < StandardError
-    attr_reader :key
-
-    def initialize(key, message)
-      super(message)
-      @key = key
-    end
-  end
-
-  before_action :checksum
   before_action :parse_metadata
-  rescue_from ApiError, with: :api_error
 
   def getRecordings
     query = Recording.includes(playback_formats: [:thumbnails], metadata: [])
@@ -103,26 +92,11 @@ class BigbluebuttonApiController < ApplicationController
 
   private
 
-  def api_error(exception)
-    @exception = exception
-    render :api_error
-  end
-
-  def checksum
-    raise ApiError.new('checksumError', 'You did not pass the checksum security check') if params[:checksum].blank?
-
-    query_string = request.query_string.gsub(/^checksum=#{params[:checksum]}&?/, '')
-    query_string = query_string.gsub(/&?checksum=#{params[:checksum]}/, '')
-    our_checksum = Digest::SHA1.hexdigest(action_name + query_string + ENV.fetch('BBB_SECRET'))
-    return if our_checksum == params[:checksum]
-
-    raise ApiError.new('checksumError', 'You did not pass the checksum security check')
-  end
-
   def parse_metadata
     @metadata = {}
     params.each do |key, value|
       next unless key.start_with?('meta_')
+
       key = key[5..-1]
       @metadata[key] = value
     end
