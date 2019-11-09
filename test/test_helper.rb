a093df01d0313ac27ae18399739f4b8281333353
Guga Salazar <guga.salazar.loor@gmail.com>
2019-11-09 18:02:37 -0300

    Use nested modules
    Style/ClassAndModuleChildren

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,17 +2,19 @@ ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 require 'rails/test_help'
 
-class ActiveSupport::TestCase
-  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
-  fixtures :all
+module ActiveSupport
+  class TestCase
+    # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
+    fixtures :all
 
-  # Add more helper methods to be used by all tests here...
-  def encode_bbb_params(api_method, query_string)
-    checksum = Digest::SHA1.hexdigest("#{api_method}#{query_string}#{ENV.fetch('BBB_SECRET')}")
-    if query_string.blank?
-      "checksum=#{checksum}"
-    else
-      "#{query_string}&checksum=#{checksum}"
+    # Add more helper methods to be used by all tests here...
+    def encode_bbb_params(api_method, query_string)
+      checksum = ::Digest::SHA1.hexdigest("#{api_method}#{query_string}#{ENV.fetch('BBB_SECRET')}")
+      if query_string.blank?
+        "checksum=#{checksum}"
+      else
+        "#{query_string}&checksum=#{checksum}"
+      end
     end
   end
 end
